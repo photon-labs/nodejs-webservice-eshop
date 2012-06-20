61a6de0597e31a511f59741c93bf97e3751a8561
arunkumar-ve <arunkumar.velusamy@photoninfotech.net>
2012-06-20 12:35:16 +0530

    [artf468949] error console

--- source/server.js
+++ source/server.js
@@ -19,38 +19,26 @@
  */
 var utility = require('./lib/utility');
 var configure = require('./lib/eshop/configure');
-
 var logger = require('./lib/logger');
 var http = require("http");
 var dbconfigure = require('./lib/eshop/dbconfigure');
 var currentEnv = process.argv[2];
 dbconfigure.configure(currentEnv);
-
 var services = require('./lib/eshop/services');
 var db = require('./lib/eshop/db');
-//var Memcached = require('memcached');
 
 var serverConfig = utility.getConfigByName(currentEnv, 'Server');
-var app = require('express').createServer();
-configure.appConfigure(app);
-services.expose(app, serverConfig);
-
-app.listen(serverConfig.port);
-
-var options = {
-  host: serverConfig.host,
-  port: serverConfig.port,
-  path: '/' + serverConfig.context
-};
-
-http.get(options, function(res) {
-  if (res.statusCode == 200) {
-    //console.log("success");
-	 var serverUrl = serverConfig.protocol + '://' + serverConfig.host 
-	+ ':' + serverConfig.port  + '/' + serverConfig.context;
-	console.log('Server running at ' + serverUrl);
-  }
-}).on('error', function(e) {
-  //console.log("Got error: " + e.message);
-  console.log("Invaldid IP Address is Configured");
-});
+require('dns').lookup(require('os').hostname(), function (err, address, fam) {
+	var ipaddress = address;
+	if (ipaddress == serverConfig.host) {
+		var app = require('express').createServer();
+		configure.appConfigure(app);
+		services.expose(app, serverConfig);
+		app.listen(serverConfig.port);
+		 var serverUrl = serverConfig.protocol + '://' + serverConfig.host 
+		+ ':' + serverConfig.port  + '/' + serverConfig.context;
+		console.log('Server running at ' + serverUrl);
+	} else {
+		  console.log("Invaldid IP Address is Configured");		
+	}
+})
