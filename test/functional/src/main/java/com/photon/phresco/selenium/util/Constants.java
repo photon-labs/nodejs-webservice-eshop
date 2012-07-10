ab25e09935e5c8bd21843cccca4d176881f41f17
pradeep <pradeep.siluveru@photoninfotech.net>
2012-07-10 14:54:17 +0530

    Removed /test folder structure from Directory variable

--- test/functional/src/main/java/com/photon/phresco/selenium/util/Constants.java
+++ test/functional/src/main/java/com/photon/phresco/selenium/util/Constants.java
@@ -54,7 +54,7 @@ public class Constants {
 	
 	public static  String MAC_OS = "Mac";
 	
-	public static String DIRECTORY="/test"+"/chromedriver";
+	public static String DIRECTORY="/chromedriver";
 	
 	public static String WINDOWS_DIRECTORY=DIRECTORY+"/windows";
 	
