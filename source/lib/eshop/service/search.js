5e0162f5d7cda3df95e8ce1434500cdb73918ec8
surendar-r <surendar.rukmangadan@photonifotech.net>
2013-03-28 12:03:06 +0530

    copyrights version changed

--- source/lib/eshop/service/search.js
+++ source/lib/eshop/service/search.js
@@ -1,3 +1,20 @@
+/*
+ * PHR_NodeJSWebService
+ *
+ * Copyright (C) 1999-2013 Photon Infotech Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 var exception = require('../../exception');
 var dbconfigure = require('../dbconfigure');
 var utility = require('../../utility');
