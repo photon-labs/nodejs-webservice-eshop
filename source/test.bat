3d09019e5c7f200f92a9eb2d9a7474c9982a5e16
saravanan-na <saravanan.natarajan@photoninfotech.net>
2014-07-14 13:20:15 +0530

    Script file to create do_not_checkin folder dir

--- source/test.bat
+++ source/test.bat
@@ -20,6 +20,7 @@ cd source
 call jscoverage lib lib-cov
 move lib lib-orig
 move lib-cov lib
+mkdir ..\do_not_checkin\target\surefire\
 call mocha -R mocha-lcov-reporter > ../do_not_checkin/target/surefire/coverage.dat
 call mocha -R xunit > ../do_not_checkin/target/surefire/TEST-AllTest.xml
 rd /S /Q lib
--- source/test.sh
+++ source/test.sh
@@ -21,6 +21,7 @@ pwd
 jscoverage lib lib-cov
 mv lib lib-orig
 mv lib-cov lib
+mkdir -p ../do_not_checkin/target/surefire
 mocha -R xunit > ../do_not_checkin/target/surefire/TEST-AllTest.xml
 mocha -R mocha-lcov-reporter > ../do_not_checkin/target/surefire/coverage.dat
 rm -rf lib
